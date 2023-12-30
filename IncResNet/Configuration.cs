4748027411b9014ee81828c8798a59537b66fb92
Bartosz Kaczorowski <baka@simcorp.com>
2023-12-30 22:37:42 +0100

    used TorchSharp documentation

---
+++ IncResNet/Configuration.cs
@@ -0,0 +1,155 @@
+﻿using System.Diagnostics;
+using TorchSharp;
+using TorchSharp.Modules;
+using static TorchSharp.torch.nn;
+using static TorchSharp.torch.nn.functional;
+using static TorchSharp.torch.utils.data;
+
+namespace IncResNet; 
+/// <summary>
+/// Custom configuration for training and evaluating models operating on 200x200 image datasets
+/// </summary>
+public static class Configuration {
+    private static int _trainBatchSize = 32;
+    private static int _testBatchSize = 64;
+    private static int _epochs = 128;
+
+    private readonly static int _loggingInterval = 25; // for logging frequency
+    private readonly static int _numClasses = 111; // number of different ages in dataset
+
+    private readonly static int _timeout = 3600; // by default an hour
+
+    public static void Main(string[] args) {
+        string datasetPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+        torch.random.manual_seed(1);
+
+        var device = torch.cuda.is_available() ? torch.CUDA : torch.CPU;
+
+        if (device.type == DeviceType.CUDA) {
+            _trainBatchSize *= 8;
+            _testBatchSize *= 8;
+            _epochs *= 8;
+        }
+
+        Console.WriteLine($"\tCreating the model...");
+        Module<torch.Tensor, torch.Tensor> model = IncResNet.IncResNetv1(device);
+
+        // override epochs and timeout with console input
+        var epochs = args.Length > 1 ? int.Parse(args[1]) : _epochs;
+        var timeout = args.Length > 2 ? int.Parse(args[2]) : _timeout;
+
+        Console.WriteLine($"\tRunning IncResNetv1 on {device.type} for {epochs} epochs, " +
+            $"terminating after {TimeSpan.FromSeconds(timeout)}.");
+
+        Console.WriteLine($"\tPreparing training and test data...");
+        Console.WriteLine();
+
+        // TODO: import nuget and load images
+        using (Dataset trainData = torchvision.datasets.ImageFolder(datasetPath, true, download: true),
+                       testData = torchvision.datasets.CIFAR100(datasetPath, false, download: true)) {
+            using var train = DataLoader(trainData, _trainBatchSize, device: device, shuffle: true);
+            using var test = DataLoader(testData, _testBatchSize, device: device, shuffle: false);
+
+            using var optimizer = torch.optim.Adam(model.parameters());
+
+            var totalSW = new Stopwatch();
+            totalSW.Start();
+
+            for (int epoch = 1; epoch <= epochs; epoch++) {
+
+                var epochSW = new Stopwatch();
+                epochSW.Start();
+
+                Train(model, optimizer, MSELoss(), train, epoch, trainData.Count);
+                Test(model, MSELoss(), test, testData.Count);
+
+                epochSW.Stop();
+                Console.WriteLine($"Elapsed time for this epoch: {epochSW.Elapsed.TotalSeconds}s.");
+
+                if (totalSW.Elapsed.TotalSeconds > timeout) { break; }
+            }
+
+            totalSW.Stop();
+            Console.WriteLine($"Elapsed training time: {totalSW.Elapsed}s.");
+        }
+        model.Dispose();
+    }
+
+    private static void Train(
+        Module<torch.Tensor, torch.Tensor> model,
+        torch.optim.Optimizer optimizer,
+        Loss<torch.Tensor, torch.Tensor, torch.Tensor> loss,
+        DataLoader dataLoader,
+        int epoch,
+        long size) {
+
+        model.train();
+
+        int batchId = 1;
+        long total = 0;
+        long correct = 0;
+
+        Console.WriteLine($"Epoch: {epoch}...");
+
+        using var d = torch.NewDisposeScope();
+        foreach (var data in dataLoader) {
+
+            optimizer.zero_grad();
+
+            var target = data["label"];
+            var prediction = model.call(data["data"]);
+            var lsm = softmax(prediction, 1);
+            var output = loss.call(lsm, target);
+
+            output.backward();
+
+            optimizer.step();
+
+            total += target.shape[0];
+
+            var predicted = prediction.argmax(1);
+            correct += predicted.eq(target).sum().ToInt64();
+
+            if (batchId % _loggingInterval == 0 || total == size) {
+                Console.WriteLine($"\rTrain: epoch {epoch} [{total} / {size}] Loss: {output.ToSingle():0.000000} | " +
+                    $"Accuracy: {((float)correct / total):0.000000}");
+            }
+            batchId++;
+
+            d.DisposeEverything();
+        }
+    }
+
+    private static void Test(
+        Module<torch.Tensor, torch.Tensor> model,
+        Loss<torch.Tensor, torch.Tensor, torch.Tensor> loss,
+        DataLoader dataLoader,
+        long size) {
+
+        model.eval();
+
+        double testLoss = 0;
+        long correct = 0;
+        int batchCount = 0;
+
+        using var d = torch.NewDisposeScope();
+        foreach (var data in dataLoader) {
+
+            var target = data["label"];
+            var prediction = model.call(data["data"]);
+            var lsm = softmax(prediction, 1);
+            var output = loss.call(lsm, target);
+
+            testLoss += output.ToSingle();
+            batchCount += 1;
+
+            var predicted = prediction.argmax(1);
+            correct += predicted.eq(target).sum().ToInt64();
+
+            d.DisposeEverything();
+        }
+        Console.WriteLine($"\rTest set: Average loss {(testLoss / batchCount):0.0000} | " +
+            $"Accuracy {((float)correct / size):0.0000}");
+    }
+}
--- IncResNet/IncResNet.cs
+++ IncResNet/IncResNet.cs
@@ -2,148 +2,152 @@
 using static TorchSharp.torch;
 using static TorchSharp.torch.nn;
 
-namespace IncResNet {
-    /// <summary>
-    /// Custom version of IncResNet for 200x200 images.
-    /// </summary>
-    public class IncResNet : Module<Tensor, Tensor> {
+namespace IncResNet;
+/// <summary>
+/// Custom version of IncResNet for 200x200 images.
+/// </summary>
+public class IncResNet : Module<Tensor, Tensor> { 
+    private readonly Module<Tensor, Tensor> _layers;
+
+    public static IncResNet IncResNetv1(Device? device = null) {
+        return new IncResNet("IncResNetv1", new int[] { 4, 8, 4 }, 768, device);
+    }
+
+    public IncResNet(string name, IList<int> numModules, int resultSize, Device? device = null) : base(name) {
+        var modules = new List<(string, Module<Tensor, Tensor>)> {
+            ($"{name}-stem", new StemBlock($"{name}-stem")),
+            ($"{name}-red-1", new ReductionBlock($"{name}-red-1", 64))
+        };
+
+        MakeLayer(name, modules, 128, numModules[0]);
+        modules.Add(($"{name}-red-2", new ReductionBlock($"{name}-red-2", 128)));
+        MakeLayer(name, modules, 256, numModules[1]);
+        modules.Add(($"{name}-red-3", new ReductionBlock($"{name}-red-3", 256)));
+        MakeLayer(name, modules, 512, numModules[2]);
+        modules.Add(($"{name}-red-4", new ReductionBlock($"{name}-red-4", 512)));
+
+        modules.Add(($"{name}-avgpool", AvgPool2d(new long[] { 5, 5 })));
+        modules.Add(($"{name}-flatten", Flatten()));
+        modules.Add(($"{name}-dropout", Dropout(p: 0.25, inplace: true)));
+        modules.Add(($"linear", Linear(inputSize: 1024, resultSize)));
+
+        _layers = Sequential(modules);
+
+        RegisterComponents();
+
+        if (device != null && device.type == DeviceType.CUDA) {
+            this.to(device);
+        }
+    }
+
+    public IncResNet(string path, Device? device = null) :
+        this("IncResNetv1", new int[] { 4, 8, 4 }, 768, device) {
+        load(path);
+    }
+
+    private static void MakeLayer(string name, List<(string, Module<Tensor, Tensor>)> modules, int filters, int numModules) {
+        for (int i = 0; i < numModules; i++) {
+            modules.Add(($"{name}-incres-{filters}-{i}", new InceptionResidualBlock($"{name}-incres-{filters}-{i}", filters)));
+        }
+    }
+
+    public override Tensor forward(Tensor input) {
+        return _layers.forward(input);
+    }
+
+    private class StemBlock : Module<Tensor, Tensor> {
         private readonly Module<Tensor, Tensor> _layers;
 
-        public static IncResNet IncResNetv1(int resultSize, Device? device = null) {
-            return new IncResNet("IncResNetv1", new int[] { 4, 8, 4 }, resultSize, device);
-        }
-
-        public IncResNet(string name, IList<int> numModules, int resultSize, Device? device = null) : base(name) {
-            var modules = new List<(string, Module<Tensor, Tensor>)> {
-                ($"{name}-stem", new StemBlock($"{name}-stem")),
-                ($"{name}-red-1", new ReductionBlock($"{name}-red-1", 64))
-            };
-
-            MakeLayer(name, modules, 128, numModules[0]);
-            modules.Add(($"{name}-red-2", new ReductionBlock($"{name}-red-2", 128)));
-            MakeLayer(name, modules, 256, numModules[1]);
-            modules.Add(($"{name}-red-3", new ReductionBlock($"{name}-red-3", 256)));
-            MakeLayer(name, modules, 512, numModules[2]);
-            modules.Add(($"{name}-red-4", new ReductionBlock($"{name}-red-4", 512)));
-
-            modules.Add(($"{name}-avgpool", AvgPool2d(new long[] { 5, 5 })));
-            modules.Add(($"{name}-flatten", Flatten()));
-            modules.Add(($"{name}-dropout", Dropout(p: 0.25, inplace: true)));
-            modules.Add(($"linear", Linear(inputSize: 1024, resultSize)));
-
-            _layers = Sequential(modules);
+        public StemBlock(string name) : base(name) {
+            _layers = Sequential(new List<(string, Module<Tensor, Tensor>)> {
+                ($"{name}-conv2d-1", Conv2d(inputChannel: 3, outputChannel: 32, kernelSize: 3, stride: 2)),
+                ($"{name}-bnrm2d-1", BatchNorm2d(32)),
+                ($"{name}-relu-1", ReLU(inplace: true)),
+                ($"{name}-conv2d-2", Conv2d(inputChannel: 32, outputChannel: 32, kernelSize: 3)),
+                ($"{name}-bnrm2d-2", BatchNorm2d(32)),
+                ($"{name}-relu-2", ReLU(inplace: true)),
+                ($"{name}-conv2d-3", Conv2d(inputChannel: 32, outputChannel: 64, kernelSize: 3, padding: 1)),
+                ($"{name}-bnrm2d-3", BatchNorm2d(64)),
+                ($"{name}-relu-3", ReLU(inplace: true))
+            });
 
             RegisterComponents();
-
-            if (device != null && device.type == DeviceType.CUDA) {
-                this.to(device);
-            }
-        }
-
-        private static void MakeLayer(string name, List<(string, Module<Tensor, Tensor>)> modules, int filters, int numModules) {
-            for (int i = 0; i < numModules; i++) {
-                modules.Add(($"{name}-incres-{filters}-{i}", new InceptionResidualBlock($"{name}-incres-{filters}-{i}", filters)));
-            }
         }
 
         public override Tensor forward(Tensor input) {
             return _layers.forward(input);
         }
-
-        private class StemBlock : Module<Tensor, Tensor> {
-            private readonly Module<Tensor, Tensor> _layers;
-
-            public StemBlock(string name) : base(name) {
-                _layers = Sequential(new List<(string, Module<Tensor, Tensor>)> {
-                    ($"{name}-conv2d-1", Conv2d(inputChannel: 3, outputChannel: 32, kernelSize: 3, stride: 2)),
-                    ($"{name}-bnrm2d-1", BatchNorm2d(32)),
-                    ($"{name}-relu-1", ReLU(inplace: true)),
-                    ($"{name}-conv2d-2", Conv2d(inputChannel: 32, outputChannel: 32, kernelSize: 3)),
-                    ($"{name}-bnrm2d-2", BatchNorm2d(32)),
-                    ($"{name}-relu-2", ReLU(inplace: true)),
-                    ($"{name}-conv2d-3", Conv2d(inputChannel: 32, outputChannel: 64, kernelSize: 3, padding: 1)),
-                    ($"{name}-bnrm2d-3", BatchNorm2d(64)),
-                    ($"{name}-relu-3", ReLU(inplace: true))
-                });
-
-                RegisterComponents();
-            }
-
-            public override Tensor forward(Tensor input) {
-                return _layers.forward(input);
-            }
-        }
-
-        private class ReductionBlock : Module<Tensor, Tensor> {
-            private readonly Module<Tensor, Tensor> _convolution;
-            private readonly Module<Tensor, Tensor> _pooling;
-
-            public ReductionBlock(string name, long filters) : base(name) {
-                _convolution = Sequential(new List<(string, Module<Tensor, Tensor>)> {
-                    ($"{name}-conv2d-1", Conv2d(inputChannel: filters, outputChannel: filters, kernelSize: 3, stride: 2)),
-                    ($"{name}-bnrm2d-1", BatchNorm2d(features: filters)),
-                    ($"{name}-relu-1", ReLU(inplace: true))
-                });
-                _pooling = MaxPool2d(kernelSize: 3, stride: 2);
-                RegisterComponents();
-            }
-
-            public override Tensor forward(Tensor input) {
-                return cat(tensors: new List<Tensor> { _convolution.forward(input), _pooling.forward(input) }, dim: 1);
-            }
-        }
-
-        private class InceptionResidualBlock : Module<Tensor, Tensor> {
-            public static int expansion = 4;
-
-            private readonly Module<Tensor, Tensor> _module1x1;
-            private readonly Module<Tensor, Tensor> _module3x3;
-            private readonly Module<Tensor, Tensor> _module5x5;
-            private readonly Module<Tensor, Tensor> _shortcut;
-
-            public InceptionResidualBlock(string name, long filters) : base(name) {
-                long filters4 = filters / 4;
-                long filters2 = filters / 2;
-
-                _module1x1 = Sequential(new List<(string, Module<Tensor, Tensor>)> {
-                    ($"{name}-conv2d-1", Conv2d(inputChannel: filters, outputChannel: filters4, kernelSize: 1)),
-                    ($"{name}-bnrm2d-1", BatchNorm2d(features: filters4)),
-                    ($"{name}-relu-1", ReLU(inplace: true))
-                });
-
-                _module3x3 = Sequential(new List<(string, Module<Tensor, Tensor>)> {
-                    ($"{name}-conv2d-1", Conv2d(inputChannel: filters, outputChannel: filters4, kernelSize: 1)),
-                    ($"{name}-bnrm2d-1", BatchNorm2d(features: filters4)),
-                    ($"{name}-relu-1", ReLU(inplace: true)),
-                    ($"{name}-conv2d-2", Conv2d(inputChannel : filters4, outputChannel: filters4, kernelSize: 3, padding: 1)),
-                    ($"{name}-bnrm2d-2", BatchNorm2d(features: filters4)),
-                    ($"{name}-relu-2", ReLU(inplace: true))
-                });
-
-                _module5x5 = Sequential(new List<(string, Module<Tensor, Tensor>)> {
-                    ($"{name}-conv2d-1", Conv2d(inputChannel: filters, outputChannel: filters4, kernelSize: 1)),
-                    ($"{name}-bnrm2d-1", BatchNorm2d(features: filters4)),
-                    ($"{name}-relu-1", ReLU(inplace: true)),
-                    ($"{name}-conv2d-2", Conv2d(inputChannel : filters4, outputChannel: filters4, kernelSize: 3, padding: 1)),
-                    ($"{name}-bnrm2d-2", BatchNorm2d(features: filters4)),
-                    ($"{name}-relu-2", ReLU(inplace: true)),
-                    ($"{name}-conv2d-3", Conv2d(inputChannel : filters4, outputChannel: filters2, kernelSize: 3, padding: 1)),
-                    ($"{name}-bnrm2d-3", BatchNorm2d(features: filters2)),
-                    ($"{name}-relu-3", ReLU(inplace: true))
-                });
-
-                _shortcut = Sequential();
-
-                RegisterComponents();
-            }
-
-            public override Tensor forward(Tensor input) {
-                var x = cat(tensors: new List<Tensor> {
-                    _module1x1.forward(input), _module3x3.forward(input), _module5x5.forward(input)
-                }, dim: 1);
-                var y = _shortcut.forward(input);
-                return x.add_(y).relu_();
-            }
-        }   
     }
+
+    private class ReductionBlock : Module<Tensor, Tensor> {
+        private readonly Module<Tensor, Tensor> _convolution;
+        private readonly Module<Tensor, Tensor> _pooling;
+
+        public ReductionBlock(string name, long filters) : base(name) {
+            _convolution = Sequential(new List<(string, Module<Tensor, Tensor>)> {
+                ($"{name}-conv2d-1", Conv2d(inputChannel: filters, outputChannel: filters, kernelSize: 3, stride: 2)),
+                ($"{name}-bnrm2d-1", BatchNorm2d(features: filters)),
+                ($"{name}-relu-1", ReLU(inplace: true))
+            });
+            _pooling = MaxPool2d(kernelSize: 3, stride: 2);
+            RegisterComponents();
+        }
+
+        public override Tensor forward(Tensor input) {
+            return cat(tensors: new List<Tensor> { _convolution.forward(input), _pooling.forward(input) }, dim: 1);
+        }
+    }
+
+    private class InceptionResidualBlock : Module<Tensor, Tensor> {
+        public static int expansion = 4;
+
+        private readonly Module<Tensor, Tensor> _module1x1;
+        private readonly Module<Tensor, Tensor> _module3x3;
+        private readonly Module<Tensor, Tensor> _module5x5;
+        private readonly Module<Tensor, Tensor> _shortcut;
+
+        public InceptionResidualBlock(string name, long filters) : base(name) {
+            long filters4 = filters / 4;
+            long filters2 = filters / 2;
+
+            _module1x1 = Sequential(new List<(string, Module<Tensor, Tensor>)> {
+                ($"{name}-conv2d-1", Conv2d(inputChannel: filters, outputChannel: filters4, kernelSize: 1)),
+                ($"{name}-bnrm2d-1", BatchNorm2d(features: filters4)),
+                ($"{name}-relu-1", ReLU(inplace: true))
+            });
+
+            _module3x3 = Sequential(new List<(string, Module<Tensor, Tensor>)> {
+                ($"{name}-conv2d-1", Conv2d(inputChannel: filters, outputChannel: filters4, kernelSize: 1)),
+                ($"{name}-bnrm2d-1", BatchNorm2d(features: filters4)),
+                ($"{name}-relu-1", ReLU(inplace: true)),
+                ($"{name}-conv2d-2", Conv2d(inputChannel : filters4, outputChannel: filters4, kernelSize: 3, padding: 1)),
+                ($"{name}-bnrm2d-2", BatchNorm2d(features: filters4)),
+                ($"{name}-relu-2", ReLU(inplace: true))
+            });
+
+            _module5x5 = Sequential(new List<(string, Module<Tensor, Tensor>)> {
+                ($"{name}-conv2d-1", Conv2d(inputChannel: filters, outputChannel: filters4, kernelSize: 1)),
+                ($"{name}-bnrm2d-1", BatchNorm2d(features: filters4)),
+                ($"{name}-relu-1", ReLU(inplace: true)),
+                ($"{name}-conv2d-2", Conv2d(inputChannel : filters4, outputChannel: filters4, kernelSize: 3, padding: 1)),
+                ($"{name}-bnrm2d-2", BatchNorm2d(features: filters4)),
+                ($"{name}-relu-2", ReLU(inplace: true)),
+                ($"{name}-conv2d-3", Conv2d(inputChannel : filters4, outputChannel: filters2, kernelSize: 3, padding: 1)),
+                ($"{name}-bnrm2d-3", BatchNorm2d(features: filters2)),
+                ($"{name}-relu-3", ReLU(inplace: true))
+            });
+
+            _shortcut = Sequential();
+
+            RegisterComponents();
+        }
+
+        public override Tensor forward(Tensor input) {
+            Tensor x = cat(tensors: new List<Tensor> {
+                _module1x1.forward(input), _module3x3.forward(input), _module5x5.forward(input)
+            }, dim: 1);
+            Tensor y = _shortcut.forward(input);
+            return x.add_(y).relu_();
+        }
+    }   
 }
