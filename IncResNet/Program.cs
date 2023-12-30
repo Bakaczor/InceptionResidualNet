1022665ebb44e10ca1c5d07ba45a49717214ebc4
Bakaczor <bartosz.kaczorowski@protonmail.com>
2023-12-30 19:38:27 +0100

    initial commit

---
+++ IncResNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using TorchSharp;
+using static TorchSharp.torch;
+
+namespace IncResNet; 
+internal class Program {
+    static void Main(string[] args) {
+        var device = new Device(DeviceType.CUDA);
+        var net = IncResNet.IncResNetv1(768, device);
+        var y = net.forward(randn(1, 3, 200, 200).cuda(device));
+        Console.WriteLine(y);
+    }
+}
